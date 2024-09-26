42967a7563763800da479b086b47f08df666a85e
01aanish <kc.01aanish@gmail.com>
2024-09-27 04:19:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,67 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Demo Project</h2>
+      <div id='inputDiv'>
+        <input placeholder='EID'/><br/>
+        <input placeholder='Name'/><br/>
+        <input placeholder='Unit'/><br/>
+        <input placeholder='Project'/>
+      </div>
+      <div id='mainDiv'>
+        <table id='newTable'>
+          <thead>
+            <tr>
+              <th>Employee Id</th>
+              <th>Name</th>
+              <th>Unit</th>
+              <th>Project</th>
+              <th>Action</th>
+            </tr>
+            <tr>
+              <td>2590874</td>
+              <td>Aanish Sharma</td>
+              <td>ESU</td>
+              <td>CIGNA</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>2590834</td>
+              <td>Nitish Gupta</td>
+              <td>ESU</td>
+              <td>CIGNA</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>3290871</td>
+              <td>Nandini</td>
+              <td>Lifescience</td>
+              <td>Lilly</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+            <tr>
+              <td>2133874</td>
+              <td>Tithi</td>
+              <td>AI Cloud</td>
+              <td>Healthcare</td>
+              <td>
+                <button>Edit</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          </thead>
+        </table>
+      </div>
     </div>
   );
 }
